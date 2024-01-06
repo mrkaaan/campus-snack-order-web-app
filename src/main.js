f86039690c94737e74c8f4aaf565af60ac72aa0d
mrkaan <mrkaan@qq.com>
2024-01-06 18:40:17 +0800

    feat: 页签图标、logo、侧边栏实际文字内容、element样式覆盖文件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 import './styles/global.scss'
+import './styles/element-overrides.scss'
 
 Vue.config.productionTip = false
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <el-container class="home-container">
     <el-aside width="200px" class="home-aside">
-      <div class="aside-top">Logo</div>
-      <el-menu class="aside-bottom" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
-        <el-menu-item v-for="i in 5" :index="i" :key="i">
-          <div class="aside-menu-item">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航{{ i }}</span>
-          </div>
-        </el-menu-item>
-      </el-menu>
+      <div class="aside-top">
+        <div class="logo-box">
+          <div class="logo" />
+        </div>
+        <div class="logo-text">
+          <span class="logo-text-ch">订餐管理后台</span>
+          <span class="logo-text-en">CampusSnackManager</span>
+        </div>
+      </div>
+      <div class="aside-bottom">
+        <el-menu class="navigation-menu-bar" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+          <el-menu-item v-for="(item, index) in navBarData" :index="index" :key="item.icon">
+            <div class="nav-menu-item" style="padding:0">
+              <i :class="item.icon"></i>
+              <span slot="title">{{ item.label }}</span>
+            </div>
+          </el-menu-item>
+        </el-menu>
+      </div>
     </el-aside>
     <el-container class="home-content">
-      <el-header class="home-header">Header</el-header>
+      <el-header class="home-header">
+        <div class="user-box">
+          <el-avatar :size="medium" :src="circleUrl"></el-avatar>
+        </div>
+      </el-header>
       <el-main class="home-main">Main</el-main>
     </el-container>
   </el-container>
@@ -24,6 +38,19 @@
 export default {
   name: 'HomeView',
   components: {
+  },
+  data () {
+    return {
+      navBarData: [
+        { icon: 'el-icon-s-home', label: '首页' },
+        { icon: 'el-icon-s-order', label: '订单管理' },
+        { icon: 'el-icon-s-management', label: '用户管理' },
+        { icon: 'el-icon-s-shop', label: '商家管理' },
+        { icon: 'el-icon-s-comment', label: '用户反馈' },
+        { icon: 'el-icon-setting', label: '系统设置' }
+      ],
+      circleUrl: ''
+    }
   }
 }
 </script>
@@ -36,8 +63,47 @@ export default {
     background-color: $primary-color;
     border-right: 1px solid $border-color;
 
+    .aside-top {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 5px 15px 15px;
+      column-gap: 10px;
+
+      .logo-box{
+        .logo{
+          border-radius:20%;
+          height: 40px;
+          width: 40px;
+          background-image: url("../assets/imgs/logo/logo-bg-thum.png");
+          background-repeat: no-repeat;
+          background-size: cover;
+        }
+      }
+
+      .logo-text {
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        text-align: left;
+
+        .logo-text-ch {
+          font-size: 20px;
+          font-weight: 800;
+        }
+        .logo-text-en {
+          font-size:12px;
+          color:#666;
+        }
+      }
+    }
     .aside-bottom{
+      display: flex;
+      justify-content: center;
       overflow: hidden;
+      .navigation-menu-bar{
+        text-align: left;
+      }
     }
   }
 
