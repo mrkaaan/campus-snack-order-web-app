2ead7e1c51b1130aee8a5e880b7e3dc9986ad7e9
mrkaan <mrkaan@qq.com>
2024-03-19 23:58:03 +0800

    feat: 添加样式服用文件multi、调整home布局样式

--- client-frontend/src/main.js
+++ client-frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 import './styles/global.scss'
+import './styles/multi.scss'
 import 'tailwindcss/tailwind.css'
 
 Vue.config.productionTip = false
--- client-frontend/src/views/HomePage.vue
+++ client-frontend/src/views/HomePage.vue
@@ -2,24 +2,24 @@
   <el-container class="home-page">
     <el-header class="page-header">
       <el-row class="header-wrapper" type="flex" justify="space-between" align="middle">
-        <el-col class="header-menu " :span="4">
-          <i class="el-icon-menu"></i>
+        <el-col class="header-menu" :span="4">
+          <i class="el-icon-menu icon-button-1 big-icon-size"></i>
         </el-col>
         <el-col :span="16" class="header-title">点餐</el-col>
         <el-col :span="4" class="header-chat">
-          <i class="el-icon-chat-line-round"></i>
+          <i class="el-icon-chat-line-round icon-button-1 big-icon-size"></i>
         </el-col>
       </el-row>
     </el-header>
     <el-main class="page-container">
       <div class="container-content">
-        <div class="content-search-bar">
-          <div class="search-wrapper">
-            <i class="el-icon-search"></i>
-            <el-input class="" placeholder="搜索食物"></el-input>
+        <div class="content-search-bar flex justify-center items-center">
+          <div class="search-wrapper flex justify-center bg-or" style="align-items: center">
+            <i class="el-icon-search icon-search big-icon-size"></i>
+            <el-input class="custom-input" placeholder="搜索食物"></el-input>
           </div>
-          <div class="search-filter">
-            <i class="el-icon-s-operation"></i>
+          <div class="search-filter flex justify-center items-center bg-or">
+            <i class="el-icon-s-operation big-icon-size"></i>
           </div>
         </div>
         <div class="content-categories">
@@ -40,47 +40,91 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .home-page {
-    background-color: #ffffff;
-    height: 100%;
+@import '../styles/multi';
 
-    .page-header {
-      .header-wrapper {
-        height: 100%;
+.home-page {
+  background-color: $primary-color;
+  height: 100%;
 
-        .header-menu, .header-menu, .header-chat {
-          height: 100%;
-          display: flex;
-          align-items: center;
-        }
-        .header-menu {
-          justify-content: left;
-        }
-        .header-chat {
-          justify-content: right;
-        }
-      }
-    }
-
-    .page-container {
+  .page-header {
+    .header-wrapper {
       height: 100%;
 
-      .container-content {
+      .header-menu, .header-menu, .header-chat {
         height: 100%;
         display: flex;
-        flex-direction: column;
+        align-items: center;
+      }
+      .header-menu {
+        justify-content: left;
+      }
+      .header-chat {
+        justify-content: right;
+      }
+    }
+  }
 
-        .content-search-bar {
-          @apply flex justify-center;
-        }
-        .content-categories {
+  .page-container {
+    height: 100%;
 
-        }
-        .content-dishes-list {
+    .container-content {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
 
+      .content-search-bar {
+        color: $orange;
+
+        column-gap: 0.8rem;
+        height: 4rem;
+
+        .search-wrapper, .search-filter {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100%;
         }
+
+        .search-wrapper {
+          flex: 1;
+          padding: 0.5rem 1rem; /* 内边距调整 */
+
+          .icon-search {
+            color: $orange;
+          }
+        }
+
+        .search-filter {
+          width: 4rem; // 根据父容器的宽度设置宽度，这里的百分比可以根据需要调整
+
+          &:hover {
+            @extend .box-shadow;
+          }
+        }
+      }
+
+      .content-categories {
+
+      }
+
+      .content-dishes-list {
 
       }
     }
   }
+}
+</style>
+
+<style lang="scss">
+@import '../styles/variables';
+
+  .custom-input .el-input__inner {
+    border: none !important;
+    background-color: transparent !important;
+  }
+
+  .content-search-bar .el-input__inner::placeholder {
+    color: $orange-light2 !important;
+  }
+
 </style>
