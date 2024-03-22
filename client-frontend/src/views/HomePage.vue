ece425050ec9c77a4d464a6b552bcea32315b66c
mrkaan <mrkaan@qq.com>
2024-03-22 17:45:18 +0800

    feat: home商品列

--- client-frontend/src/views/HomePage.vue
+++ client-frontend/src/views/HomePage.vue
@@ -29,13 +29,38 @@
             </div>
           </div>
         </div>
-        <div class="content-dishes-list">
+        <div class="content-dishes-list" v-if="isLoading">
           <ul class="product-list">
-            <li v-for="item in products" :key="item">
+            <li class="skeleton-item" v-for="n in skeletonCount" :key="`skeleton-${n}`">
+              <el-skeleton animated :throttle="500">
+                <template slot="template">
+                  <div class="skeleton-template">
+                    <div class="skeleton-image">
+                      <el-skeleton-item class="skeleton-img" variant="image"></el-skeleton-item>
+                    </div>
+                    <div class="item-detail">
+                      <el-skeleton-item variant="text" ></el-skeleton-item>
+                      <el-skeleton-item variant="text" ></el-skeleton-item>
+                      <el-skeleton-item variant="text"></el-skeleton-item>
+                    </div>
+                  </div>
+                </template>
+              </el-skeleton>
+            </li>
+          </ul>
+        </div>
+        <div class="content-dishes-list" v-else>
+          <ul class="product-list">
+            <li  v-for="item in products" :key="item.id">
               <a class="product-item-link">
                 <div class="product-item">
-                  <div class="item-img">img</div>
-                  <div class="item-detail">detail</div>
+                  <div class="item-img">
+                    <el-image class="image" :src="item.image" fit="cover" @load="() => item.imageLoaded = true"></el-image>
+                  </div>
+                  <div class="item-detail">
+                    <p><strong>{{ item.storeName }}</strong></p>
+                    <p><strong>{{ item.price }}</strong></p>
+                  </div>
                 </div>
               </a>
             </li>
@@ -66,11 +91,76 @@ export default {
         { name: 'category', value: 3 }
         // 更多分类...
       ],
-      products: 10
+      products: [],
+      MockProducts: [
+        {
+          id: 0,
+          storeName: '脆皮鸡排',
+          price: '￥45.00',
+          image: 'https://via.placeholder.com/150?text=0',
+          imageLoaded: true
+        },
+        {
+          id: 1,
+          storeName: '烤羊排',
+          price: '￥60.00',
+          image: 'https://via.placeholder.com/150?text=1',
+          imageLoaded: true
+        },
+        {
+          id: 2,
+          storeName: '香辣鱿鱼',
+          price: '￥20.00',
+          image: 'https://via.placeholder.com/150?text=2',
+          imageLoaded: true
+        },
+        {
+          id: 3,
+          storeName: '烤羊排',
+          price: '￥51.00',
+          image: 'https://via.placeholder.com/150?text=3',
+          imageLoaded: true
+        },
+        {
+          id: 4,
+          storeName: '烤羊排',
+          price: '￥97.00',
+          image: 'https://via.placeholder.com/150?text=4',
+          imageLoaded: true
+        }
+      ],
+      isLoading: true, // 初始时数据正在加载
+      skeletonCount: 5 // 假设初始加载显示5个骨架屏
     }
   },
   components: {
     CartFloating
+  },
+  mounted () {
+    // this.fetchProducts() // 组件挂载后加载数据
+    this.MockFetchProducts()
+  },
+  methods: {
+    async fetchProducts () {
+      // 模拟数据加载
+      this.isLoading = true // 开始加载数据
+      try {
+        // const response = await fetchProductsFromAPI() // 假设的API请求方法
+        // this.products = response.data
+      } catch (error) {
+        console.error('Failed to fetch products:', error)
+      } finally {
+        this.isLoading = false // 完成加载
+      }
+    },
+
+    MockFetchProducts () {
+      // 使用setTimeout来模拟数据的异步加载
+      setTimeout(() => {
+        this.products = this.MockProducts // 更新products数据
+        this.isLoading = false // 数据加载完成，更新加载状态
+      }, 2000) // 延迟2秒来模拟网络请求延迟
+    }
   }
 }
 </script>
@@ -182,21 +272,71 @@ export default {
           flex-direction: column;
           row-gap: 1.5rem;
 
+          .skeleton-item {
+            .skeleton-template {
+              width: 100%;
+              height: 10rem;
+              border-radius: 1rem;
+              display: flex;
+              align-items: center;
+              flex-direction: row;
+              justify-content: start;
+              @extend .box-shadow-2;
+            }
+
+            .skeleton-image {
+              width: 10rem; /* 根据实际图片大小调整 */
+              height: 100%; // 保持图片的高度与项目一致
+              object-fit: cover; // 确保图片覆盖整个区域
+              border-radius: 1rem 0 0 1rem; // 为图片添加圆角，仅左侧
+              overflow: hidden;
+
+              .skeleton-img {
+                height: 100%;
+                width: 100%;
+              }
+            }
+
+            .item-detail {
+              height: 100%;
+              display: flex;
+              row-gap: 1rem;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+              padding: 0 1rem;
+              flex: 1;
+            }
+          }
+
           .product-item{
             width: 100%;
             height: 10rem;
             border-radius: 1rem;
             display: flex;
             flex-direction: row;
-            justify-content: center;
+            justify-content: start;
             align-items: center;
             @extend .box-shadow-2;
 
             .item-img {
               width: 10rem;
+              height: 100%; // 保持图片的高度与项目一致
+              object-fit: cover; // 确保图片覆盖整个区域
+              border-radius: 1rem 0 0 1rem; // 为图片添加圆角，仅左侧
+              overflow: hidden;
+              .image {
+                height: 100%;
+                width: 100%;
+              }
             }
             .item-detail {
-              flex: 1
+              flex: 1;
+              padding-left: 1rem; // 给详情文本添加一些左边距
+
+              p {
+                margin: 0.5rem 0; // 给文本添加一些间距
+              }
             }
           }
         }
