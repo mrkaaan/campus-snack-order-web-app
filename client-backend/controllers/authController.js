b4c0c1678b801a2edd5f8c3bec869ceeb4ec643b
mrkaan <mrkaan@qq.com>
2024-05-10 22:46:55 +0800

    feat: 接口支持商家注册和登录

--- client-backend/controllers/authController.js
+++ client-backend/controllers/authController.js
@@ -3,13 +3,10 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const emailService = require('../services/emailService'); // 调整路径以正确导入
-// const jwtSecret = 'my_jwt_secret';
 
 function decryptAES(encryptedText) {
   const key = Buffer.from(process.env.AES_KEY, 'hex'); // 将密钥从十六进制字符串转换为Buffer对象
-  console.log(key)
   const iv = Buffer.from(process.env.AES_IV, 'hex'); // 将初始向量从十六进制字符串转换为Buffer对象
-  console.log(iv)
   let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv); // 创建解密器实例，设置为AES-256-CBC
   let decrypted = decipher.update(encryptedText, 'base64', 'utf8'); // 解密过程，将加密文本从base64转为utf8
   decrypted += decipher.final('utf8'); // 完成解密过程
@@ -26,9 +23,7 @@ function generateAccountId(text) {
 // 注册新用户
 exports.register = async (req, res) => {
 
-  const { data, registerType } = req.body;
-  console.log(data)
-  console.log(registerType)
+  const { data, registerType, isMerchant  } = req.body;
   // 解密数据
   // let userData;
   // try {
@@ -38,33 +33,36 @@ exports.register = async (req, res) => {
   //   return res.status(500).json({ success: false, message: "注册失败 验证码已过期", data: null });
   // }
   const userData = data
+  const table = isMerchant ? 'MerchantUsers' : 'Users'; // 定义查询和插入的表格
   try {
     if (registerType === 'normal') {
-      // 检查用户名是否已存在
       const { username, password } = userData;
 
-      // 判空
+      // 检查输入字段是否为空
       if (!username || !password) {
         return res.status(400).json({
           success: false,
-          message: "用户名或密码为空",
+          message: "必要信息不能为空",
           data: null
         });
       }
 
       // 检查用户名是否已存在
-      const [userExists] = await db.query("SELECT accountId FROM Users WHERE username = ?", [username]);
+      const userCheckQuery = `SELECT accountId FROM ${table} WHERE username = ?`;
+      const [userExists] = await db.query(userCheckQuery, [username]);
       if (userExists.length > 0) {
         return res.status(409).json({ success: false, message: "用户名已存在" });
       }
 
       // 注册用户
-      const hashedPassword = await bcrypt.hash(password, 10); // 验证密码
+      // const hashedPassword = await bcrypt.hash(password, 10); // 密码加密
+      const hashedPassword = password
       let accountId = generateAccountId(username); // 生成accountId
-      let exists = true;
+      let exists = true; // 判断accountId是否存在
       let attempts = 0;
       while (exists && attempts < 10) { // 限制尝试次数以避免无限循环
-        const [rows] = await db.query("SELECT accountId FROM Users WHERE accountId = ?", [accountId]);
+        const idCheckQuery = `SELECT accountId FROM ${table} WHERE accountId = ?`;
+        const [rows] = await db.query(idCheckQuery, [accountId]);
         if (rows.length === 0) {
           exists = false;
         } else {
@@ -72,8 +70,8 @@ exports.register = async (req, res) => {
           attempts++;
         }
       }
-
-      await db.query("INSERT INTO Users (accountId, username, password) VALUES (?, ?, ?)", [accountId, username, hashedPassword]);
+      const insertUsersQuery = `INSERT INTO ${table} (accountId, username, password) VALUES (?, ?, ?)`;
+      await db.query(insertUsersQuery, [accountId, username, hashedPassword]);
 
     } else if (registerType === 'email') {
       const { email, verCode } = userData;
@@ -86,15 +84,15 @@ exports.register = async (req, res) => {
         });
       }
       // 检查邮箱是否已存在
-      const [userExists] = await db.query("SELECT accountId FROM Users WHERE email = ?", [email]);
+      const checkEmailQuery = `SELECT accountId FROM ${table} WHERE email = ?`;
+      const [userExists] = await db.query(checkEmailQuery, [email]);
       if (userExists.length > 0) {
         return res.status(409).json({ success: false, message: "邮箱已被注册" });
       }
 
       // 检查验证码是否存在
-      const [verifications] = await db.query(`
-            SELECT verification_code, expiry_time FROM EmailVerifications
-            WHERE email = ?`, [email]);
+      const checkVerificationQuery = `SELECT verification_code, expiry_time FROM EmailVerifications WHERE email = ?`;
+      const [verifications] = await db.query(checkVerificationQuery, [email]);
       const verification = verifications ? verifications[0] : null;
       if (!verification) {
         return res.status(400).json({ success: false, message: "请发送验证码" });
@@ -108,17 +106,16 @@ exports.register = async (req, res) => {
       }
 
       // 验证码验证通过后，删除验证码记录
-      await db.query("DELETE FROM EmailVerifications WHERE email = ?", [email]);
-
-      // 生成八位随机数作为UID
-      const uid = Math.floor(10000000 + Math.random() * 90000000).toString();
+      const deleteVerificationQuery = `DELETE FROM EmailVerifications WHERE email = ?`;
+      await db.query(deleteVerificationQuery, [email]);
 
       // 生成随机accountId
       let accountId = generateAccountId(email);
       let exists = true;
       let attempts = 0;
       while (exists && attempts < 10) { // 限制尝试次数以避免无限循环
-        const [rows] = await db.query("SELECT accountId FROM Users WHERE accountId = ?", [accountId]);
+        const idCheckQuery = `SELECT accountId FROM ${table} WHERE accountId = ?`;
+        const [rows] = await db.query(idCheckQuery, [accountId]);
         if (rows.length === 0) {
           exists = false;
         } else {
@@ -128,7 +125,8 @@ exports.register = async (req, res) => {
       }
 
       // 注册用户
-      await db.query("INSERT INTO Users (accountId, email) VALUES (?, ?)", [accountId, email]);
+      const insertUsersQuery = `INSERT INTO ${table} (accountId, email) VALUES (?, ?)`;
+      await db.query(insertUsersQuery, [accountId, email]);
 
     } else {
       return res.status(400).json({ success: false, message: "无效的注册类型" });
@@ -141,60 +139,13 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.registerDemo = async (req, res) => {
-  try {
-    console.log('registerDemo')
-    // 检查邮箱是否已存在
-    const [rows] = await db.query("SELECT accountId FROM Users WHERE email = ?", ['196022210@qq.com']);
-    console.log('rows:', rows)
-    const user = rows ? rows[0] : null;
-    if (user) {
-      console.log('User already exists')
-    } else {
-      console.log('User does not exist')
-    }
-
-    const [verifications, _f] = await db.query(`
-              SELECT verification_code, expiry_time FROM EmailVerifications
-              WHERE email = ?`, ['1960222170@qq.com']);
-    console.log('verifications:', verifications)
-    vercc = verifications[0]
-    console.log('vercc:', vercc)
-    if (!vercc) {
-      console.log('No verification code found')
-    } else {
-      console.log('Verification code found')
-    }
-
-    if (!verifications){
-      console.log('No verification code found')
-    } else {
-      console.log('Verification code found')
-    }
-
-    const [existingUser] = await db.query("SELECT * FROM Users WHERE username = ?", ['demo123']);
-    console.log('Existing user check:', existingUser);
-    res.status(200).json({
-      success: true,
-      message: "successfully connected",
-      data: existingUser
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error,
-      data: null
-    });
-  }
-}
-
 // 判断用户身份并查遍检查该用户在数据库中是否存在
-function queryDatabaseForLogin(userinfo) {
+function queryDatabaseForLogin(userinfo, table) {
   // 这里需要根据userinfo判断是email、username还是UID
   let userType = null;
-  const queryAccountId = 'SELECT * FROM Users WHERE accountId = ?';
-  const queryUsername = 'SELECT * FROM Users WHERE username = ?';
-  const queryEmail = 'SELECT * FROM Users WHERE email = ?';
+  const queryAccountId = `SELECT * FROM ${table} WHERE accountId = ?`
+  const queryUsername = `SELECT * FROM ${table} WHERE username = ?`
+  const queryEmail = `SELECT * FROM ${table} WHERE email = ?`
 
   // 简单的正则判断
   if (/^\d{8}$/.test(userinfo)) { // 假设UID是8位数字
@@ -211,11 +162,10 @@ function queryDatabaseForLogin(userinfo) {
 
 // 用户登录
 exports.login = async (req, res) => {
-  const { userData, loginType } = req.body;
-  console.log('req.body:',  req.body)
-  console.log('userData:', userData)
-  console.log('loginType:', loginType)
+  const { userData, loginType, isMerchant } = req.body;
   try {
+    const table = isMerchant ? 'MerchantUsers' : 'Users'; // 定义查询和插入的表格
+
     if (loginType === 'normal') {
       const { userinfo, password } = userData
 
@@ -229,8 +179,7 @@ exports.login = async (req, res) => {
       }
 
       // 检查用户名是否已存在
-
-      const [query, userType] =  queryDatabaseForLogin(userinfo)
+      const [query, userType] =  queryDatabaseForLogin(userinfo, table)
       const [rows] = await db.query(query, [userinfo])
       const user = rows ? rows[0] : null;
       if (!user) {
@@ -241,23 +190,19 @@ exports.login = async (req, res) => {
         });
       }
       if (!user.password) {
-        console.log('该用户未设置密码，请使用邮箱验证码登录')
         return res.status(401).json({
           success: false,
           message: "该用户未设置密码，请使用邮箱验证码登录"
         });
       }
 
-      let passwordMatch = false
       // 验证密码
-      if (user.role === 'user') {
-        passwordMatch = await bcrypt.compare(password, user.password);
-      } else {
-        passwordMatch = password === user.password;
-      }
+      // const passwordMatch = await bcrypt.compare(password, user.password);
+      const passwordMatch = password === user.password
       if (passwordMatch) {
         const expiresIn = userData.remember ? '7d' : '1d';
-        const token = jwt.sign({ userinfo: userinfo, mode: 'normal' }, process.env.JWT_SECRET, { expiresIn }); // 使用JWT生成token
+        const token = jwt.sign({ userinfo: userinfo }, process.env.JWT_SECRET, { expiresIn }); // 使用JWT生成token
+        const mode = isMerchant ? 'merchant' : 'user';
         res.status(200).json({
           success: true,
           message: "登录成功",
@@ -268,9 +213,8 @@ exports.login = async (req, res) => {
               email: user.email || null,
               hasPassword: !!user.password
             },
-            mode:'normal', // 登录模式 normal 或者 guest
+            mode: mode,
             token,
-            isAdmin: user.role === 'admin',
           }
         });
       } else {
@@ -293,7 +237,8 @@ exports.login = async (req, res) => {
       }
 
       // 检查邮箱是否已存在
-      const [rows] = await db.query("SELECT * FROM Users WHERE email = ?", [email]);
+      const checkEmail = `SELECT * FROM ${table} WHERE email = ?`;
+      const [rows] = await db.query(checkEmail, [email]);
       const user = rows ? rows[0] : null;
       if (!user) {
         // 邮箱未注册 自动注册
@@ -317,7 +262,8 @@ exports.login = async (req, res) => {
         // 验证码验证通过后，删除验证码记录
         await db.query("DELETE FROM EmailVerifications WHERE email = ?", [email]);
         const expiresIn = userData.remember ? '7d' : '1d';
-        const token = jwt.sign({ email: user.email, mode:'normal' }, process.env.JWT_SECRET, { expiresIn }); // 使用JWT生成token
+        const token = jwt.sign({ userinfo: user.email }, process.env.JWT_SECRET, { expiresIn }); // 使用JWT生成token
+        const mode = isMerchant ? 'merchant' : 'user';
         res.status(200).json({
           success: true,
           message: "登录成功",
@@ -328,7 +274,7 @@ exports.login = async (req, res) => {
               email: user.email || null,
               hasPassword: !!user.password
             },
-            mode:'normal', // 登录模式 normal 或者 guest
+            mode: mode, // 登录模式 normal 或者 guest
             token
           }
         });
@@ -350,30 +296,31 @@ exports.logout = async (req, res) => {
   const { id } = jwt.verify(req.cookies.token, 'your_jwt_secret');
   await db.query("UPDATE Users SET token = NULL WHERE id = ?", [id]);
   res.clearCookie('rememberMe');
-  res.status(200).json({ message: "Logged out successfully" });
+  res.status(200).json({ message: "注销成功" });
 };
 
+// 游客登录
 exports.loginAsGuest = async (req, res) => {
   try {
     // 生成一个游客JWT
-    const token = jwt.sign({
-      mode: 'guest',
-    }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    const token = jwt.sign({ userinfo: 'guest' }, process.env.JWT_SECRET, { expiresIn: '24h' });
 
     // 返回生成的游客JWT
     res.status(200).json({
       success: true,
-      message: "Guest login successful",
+      message: "游客登录成功",
       data: {
+        user: null,
+        mode: 'user',
         token,
-        mode: 'guest'
+        isGuest: true
       }
     });
   } catch (error) {
     console.error("Error during guest login process:", error);
     res.status(500).json({
       success: false,
-      message: "Error logging in as guest",
+      message: "游客登录失败",
       data: null
     });
   }
@@ -383,7 +330,6 @@ exports.loginAsGuest = async (req, res) => {
 exports.sendEmailVerificationCode = async (req, res) => {
   try {
     const { email } = req.body;
-    console.log("Received email:", email)
     if (!email) {
       return res.status(400).json({
         success: false,
@@ -391,7 +337,8 @@ exports.sendEmailVerificationCode = async (req, res) => {
       });
     }
     const verificationCode = Math.floor(100000 + Math.random() * 900000); // 生成6位随机数作为验证码
-    console.log("Generated verification code:", verificationCode)
+    console.log("Sending verification email to:", email, verificationCode)
+    console.log("Sending...")
     await emailService.sendVerificationEmail(email, verificationCode.toString());
 
     const expiryTime = new Date(Date.now() + 5 * 60 * 1000); // 设置验证码5分钟后过期
@@ -409,6 +356,7 @@ exports.sendEmailVerificationCode = async (req, res) => {
       message: "验证码已发送",
       data: null
     });
+    console.log("Verification email sent successfully")
   } catch (error) {
     console.error("Failed to send verification email:", error);
     res.status(500).json({
--- client-backend/routes/authRoutes.js
+++ client-backend/routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { login, register,registerDemo, logout, loginAsGuest, sendEmailVerificationCode  } = require('../controllers/authController');
+const { login, register, logout, loginAsGuest, sendEmailVerificationCode  } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/login').post(login);
 router.route('/register').post(register);
 router.route('/logout').post(logout);
-router.route('/registerDemo').get(registerDemo);
 router.route('/send-verification-email').post(sendEmailVerificationCode);
 router.route('/login-guest').post(loginAsGuest);
 
